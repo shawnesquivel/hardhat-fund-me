0f7b5e794b2967d73ff6c502a37d1959c6112f58
Shawn Esquivel <shawnesquivel24@gmail.com>
2022-12-18 18:42:59 -0800

    added fundMe test for multiple funders

--- test/unit/FundMe.test.js
+++ test/unit/FundMe.test.js
@@ -120,5 +120,50 @@ describe("FundMe", async function () {
                 endingDeployerBalance.toString()
             )
         })
+
+        it("allows us to withdraw with multiple funders", async function () {
+            const accounts = await ethers.getSigners()
+
+            for (let i = 1; i < 6; i++) {
+                accounts[i]
+            }
+
+            await fundMeConnectedContract.fund({ value: sendValue })
+
+            const startingFundMeBalance = await fundMe.provider.getBalance(
+                fundMe.address
+            )
+            const startingDeployerBalance = await fundMe.provider.getBalance(
+                deployer
+            )
+
+            const transactionResponse = await fundMe.withdraw()
+
+            const transactionReceipt = await transactionResponse.wait(1)
+
+            const endingFundMeBalance = await fundMe.provider.getBalance(
+                fundMe.address
+            )
+            const endingDeployerBalance = await fundMe.provider.getBalance(
+                deployer
+            )
+
+            assert.equal(endingFundMeBalance, 0)
+            // Ending_Balance = Starting_Contract + Starting_Balance - Gas Fees
+            assert.equal(
+                startingFundMeBalance
+                    .add(startingDeployerBalance)
+                    .sub(gasSpent),
+                endingDeployerBalance.toString()
+            )
+
+            await expect(fundMe.funders(0)).to.be.reverted
+
+            for (i = 1; i < 6; i++) {
+                assert.equal(
+                    await fundMe.addressToAmountFunded(accounts[i].address)
+                )
+            }
+        })
     })
 })
